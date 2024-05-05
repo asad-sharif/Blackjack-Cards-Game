18d4ea1313c33e24fc56f1bf5e9014a13ad6e1f0
Asad Sharif <asadsharif1270@gmail.com>
2024-05-05 22:07:12 +0500

    completed

--- script.js
+++ script.js
@@ -1,33 +1,77 @@
-//grabbing DOM nodes
+// Grabbing DOM nodes
 const newDeckButton = document.getElementById('new-deck-btn')
 const drawButton = document.getElementById('draw-btn')
+const cardsContainer = document.getElementById("cards")
+const header = document.getElementById("header")
+const remainingText = document.getElementById("remaining")
+const computerScoreEl = document.getElementById("computer-score")
+const myScoreEl = document.getElementById("my-score")
 
-let deckID
+let deckId
+let computerScore = 0
+let myScore = 0
 
-newDeckButton.addEventListener('click', () => {
+// Function to handle new deck button click
+function handleNewDeckClick() {
     fetch('https://deckofcardsapi.com/api/deck/new/shuffle/')
         .then(response => response.json())
         .then(data => {
-            // console.log(data)
-            deckID = data.deck_id
-            console.log(deckID);
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
+            deckId = data.deck_id
+            console.log(deckId);
             drawButton.disabled = false
             newDeckButton.disabled = true
         })
-})
+}
 
-drawButton.addEventListener('click', () => {
-    fetch(`https://deckofcardsapi.com/api/deck/${deckID}/draw/?count=2`)
+// Function to handle draw card button click
+function handleDrawClick() {
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
         .then(res => res.json())
         .then(data => {
-            // console.log(data.cards);
-
-            document.getElementById('cards').innerHTML = `
+            remainingText.textContent = `Remaining cards: ${data.remaining}`
+            cardsContainer.children[0].innerHTML = `
                 <img src=${data.cards[0].image} class="card" />
-                <img src=${data.cards[1].image} class="card" />`
-
-            // let cardImg = data.Image
-            // console.log(cardImg);
+            `
+            cardsContainer.children[1].innerHTML = `
+                <img src=${data.cards[1].image} class="card" />
+            `
+            const winnerText = determineCardWinner(data.cards[0], data.cards[1])
+            header.textContent = winnerText
+            
+            if (data.remaining === 0) {
+                drawButton.disabled = true
+                if (computerScore > myScore) {
+                    header.textContent = "The computer won the game!"
+                } else if (myScore > computerScore) {
+                    header.textContent = "You won the game!"
+                } else {
+                    header.textContent = "It's a tie game!"
+                }
+            }
         })
-})
+}
 
+// Event listeners
+newDeckButton.addEventListener('click', handleNewDeckClick)
+drawButton.addEventListener('click', handleDrawClick)
+
+// Function to determine the winner
+function determineCardWinner(card1, card2) {
+    const valueOptions = ["2", "3", "4", "5", "6", "7", "8", "9", 
+    "10", "JACK", "QUEEN", "KING", "ACE"]
+    const card1ValueIndex = valueOptions.indexOf(card1.value)
+    const card2ValueIndex = valueOptions.indexOf(card2.value)
+    
+    if (card1ValueIndex > card2ValueIndex) {
+        computerScore++
+        computerScoreEl.textContent = `Computer score: ${computerScore}`
+        return "Computer wins!"
+    } else if (card1ValueIndex < card2ValueIndex) {
+        myScore++
+        myScoreEl.textContent = `My score: ${myScore}`
+        return "You win!"
+    } else {
+        return "War!"
+    }
+}
